4eb8fa53329eaae01be1ef7c7eb5be33d9f30695
Thayna-AS <thayna.almeida.silva23@gmail.com>
2023-06-19 10:00:43 -0300

    Adição de Footer, Navbar e rotas

--- blogpessoal/src/App.tsx
+++ blogpessoal/src/App.tsx
@@ -1,25 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import Navbar from './componentes/estaticos/navbar/Navbar';
+import Footer from './componentes/estaticos/footer/footer';
+import Home from './paginas/home/home';
+import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/login/Login';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return(
+  return (
     <BrowserRouter>
-    <Navbar />
-    <div style={{ minHeight: '100vh' }}>
-    <Routes> // Antigo Switch
-    <Route path="/" element={<Login />} />
-    <Route path="/login" element={<Login />} />
-    <Route path="/home" element={<Home />} />
-    <Route path="/cadastro" element={<CadastroUsuario />} />
-    </Routes>
-    </div>
-    <Footer />
+      <Navbar />
+      <div style={{ minHeight: '65vh' }}>
+        <Routes> // Antigo Switch
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          {/* <Route path="/cadastro" element={<CadastroUsuario />} /> */}
+        </Routes>
+      </div>
+      <Footer />
     </BrowserRouter>
-    )
-    
-  }
-export default App
+  );
+}
+
+export default App;
---
+++ blogpessoal/src/componentes/estaticos/footer/footer.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import GitHubIcon from '@mui/icons-material/Github';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import {Typography, Grid } from '@material-ui/core';
+import {Box} from '@mui/material';
+
+function Footer(){
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center">
+                <Grid alignItems="center" item xs={12}>
+                    <Box style={{ backgroundColor: "#2D2D2D", height: "120px" }}>
+                        <Box paddingTop={1} display="flex" alignItems="center" justifyContent="center">
+                            <Typography variant="h5" align="center" gutterBottom style={{ color: "lightgrey" }}>Me acompanhe nas redes </Typography>
+                        </Box>
+                        <Box display="flex" alignItems="center" justifyContent="center">
+                            <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                                <GitHubIcon style={{ fontSize: 50, color: "white" }} />
+                            </a>
+                            <a href="https://www.linkedin.com/school/generationbrasil/" target="_blank">
+                                <LinkedInIcon style={{ fontSize: 60, color: "white" }} />
+                            </a>
+                        </Box>
+                    </Box>
+                    <Box style={{ backgroundColor: "#000000", height: "40px" }}>
+                        <Box paddingTop={1}>
+                            <Typography variant="subtitle2" align="center" gutterBottom style={{ color: "lightgrey" }} >© 2023 Desenvolvido por: Luiz Fernando Benedito Rodrigues</Typography>
+                        </Box>
+                    </Box>
+                </Grid>
+            </Grid>
+        </>
+    )
+}
+
+export default Footer;
